bf1f6c27fa524c54b281281aca6cfcbbf2a6b03b
CeciliaMarson <cecilia.marson@gmail.com>
2020-05-16 16:44:38 +0200

    Add files via upload

--- spectrumplugin2/Source/PluginEditor.cpp
+++ spectrumplugin2/Source/PluginEditor.cpp
@@ -14,36 +14,107 @@
 
 //==============================================================================
 Spectrumplugin2AudioProcessorEditor::Spectrumplugin2AudioProcessorEditor (Spectrumplugin2AudioProcessor& p)
-    : AudioProcessorEditor (&p), processor (p)
+    : AudioProcessorEditor (&p), processor (p), forwardFFT(fftOrder),
+    spectrogramImage(Image::RGB, 400, 300, true), fifoIndex(0), nextFFTBlockReady(false) 
     
 {
-    addAndMakeVisible(&m_SpectroGramComp);
+    //addAndMakeVisible(&m_SpectroGramComp);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
-    
-    
+    setOpaque(true);
+    startTimerHz(60);
+    //setAudioChannels(2, 0);  // we want a couple of input channels but no outputs   
     
 }
+    
+
+
+
 
 Spectrumplugin2AudioProcessorEditor::~Spectrumplugin2AudioProcessorEditor()
 {
+    
 }
+
+void Spectrumplugin2AudioProcessorEditor::processAudioBlock(const AudioBuffer<float>& buffer)
+	{
+		if (buffer.getNumChannels() > 0)
+		{
+			const float* channelData = buffer.getReadPointer(0);
+			for (int i = 0; i < buffer.getNumSamples(); ++i)
+				pushNextSampleIntoFifo(channelData[i]);
+		}
+	}
 
 //==============================================================================
 void Spectrumplugin2AudioProcessorEditor::paint (Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (Colours::green);
-    
-    
+    g.fillAll (Colours::black);
+
+    g.setOpacity (1.0f);
+    g.drawImage (spectrogramImage, getLocalBounds().toFloat());
 }
 
 void Spectrumplugin2AudioProcessorEditor::resized()
 {
-    m_SpectroGramComp.setBounds(0, 0, getWidth(), getHeight());
+    //m_SpectroGramComp.setBounds(0, 0, getWidth(), getHeight());
 
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
 }
 
+void Spectrumplugin2AudioProcessorEditor::timerCallback() 
+    {
+        if (nextFFTBlockReady)
+        {
+            drawNextLineOfSpectrogram();
+            nextFFTBlockReady = false;
+            repaint();
+        }
+    }
+
+void Spectrumplugin2AudioProcessorEditor::pushNextSampleIntoFifo (float sample) noexcept
+    {
+        // if the fifo contains enough data, set a flag to say
+        // that the next line should now be rendered..
+        if (fifoIndex == fftSize)
+        {
+            if (! nextFFTBlockReady)
+            {
+                zeromem (fftData, sizeof (fftData));
+                memcpy (fftData, fifo, sizeof (fifo));
+                nextFFTBlockReady = true;
+            }
+
+            fifoIndex = 0;
+        }
+
+        fifo[fifoIndex++] = sample;
+    }
+
+void Spectrumplugin2AudioProcessorEditor::drawNextLineOfSpectrogram()
+    {
+        auto rightHandEdge = spectrogramImage.getWidth() - 1;
+        auto imageHeight   = spectrogramImage.getHeight();
+
+        // first, shuffle our image leftwards by 1 pixel..
+        spectrogramImage.moveImageSection (0, 0, 1, 0, rightHandEdge, imageHeight);
+
+        // then render our FFT data..
+        forwardFFT.performFrequencyOnlyForwardTransform (fftData);
+
+        // find the range of values produced, so we can scale our rendering to
+        // show up the detail clearly
+        auto maxLevel = FloatVectorOperations::findMinAndMax (fftData, fftSize / 2);
+
+        for (auto y = 1; y < imageHeight; ++y)
+        {
+            auto skewedProportionY = 1.0f - std::exp (std::log (y / (float) imageHeight) * 0.2f);
+            auto fftDataIndex = jlimit (0, fftSize / 2, (int) (skewedProportionY * fftSize / 2));
+            auto level = jmap (fftData[fftDataIndex], 0.0f, jmax (maxLevel.getEnd(), 1e-5f), 0.0f, 1.0f);
+
+            spectrogramImage.setPixelAt (rightHandEdge, y, Colour::fromHSV (level, 1.0f, level, 1.0f));
+        }
+    }
--- spectrumplugin2/Source/PluginEditor.h
+++ spectrumplugin2/Source/PluginEditor.h
@@ -18,8 +18,8 @@
 /**
 */
 
-class SpectrogramComponent   : public Component,
-                               private Timer
+/*class SpectrogramComponent   : public Component,
+                                   private Timer
 {
 public:
     SpectrogramComponent()
@@ -133,8 +133,9 @@ private:
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SpectrogramComponent)
 };
-
-class Spectrumplugin2AudioProcessorEditor  : public AudioProcessorEditor
+*/
+class Spectrumplugin2AudioProcessorEditor  : public AudioProcessorEditor,
+                                             private Timer
                                 
 {                                   
 public:
@@ -144,14 +145,33 @@ public:
     //==============================================================================
     void paint (Graphics&) override;
     void resized() override;
-    SpectrogramComponent m_SpectroGramComp;
+    //SpectrogramComponent m_SpectroGramComp;
+    void processAudioBlock(const AudioBuffer<float>& buffer);
+    void timerCallback() override;
+    void pushNextSampleIntoFifo (float sample) noexcept;
+    void drawNextLineOfSpectrogram();
+    
+        enum
+    {
+        fftOrder = 10,
+        fftSize  = 1 << fftOrder
+    };
+    
+    
+    dsp::FFT forwardFFT;
+    Image spectrogramImage;
+
+    float fifo [fftSize];
+    float fftData [2 * fftSize];
+    int fifoIndex = 0;
+    bool nextFFTBlockReady = false;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Spectrumplugin2AudioProcessor& processor;
 
-    
+   
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Spectrumplugin2AudioProcessorEditor)
 };
